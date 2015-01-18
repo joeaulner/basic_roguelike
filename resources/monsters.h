dacb5b6df588d8fed6bd974aaaba1dfaa564589c
pancakeCaptain <jaaulner@gmail.com>
2015-01-18 03:22:36 +0000

    Reimplemented map representation with multiple arrays

--- resources/monsters.h
+++ resources/monsters.h
@@ -5,29 +5,30 @@
   structs or if the one struct will be sufficient. The monster AI's may also be
   included in this file, but they could be stored in their own files as well.
  ******************************************************************************/
-#ifndef MONSTER_RESOURCES_H
-#define MONSTER_RESOURCES_H
+#ifndef MONSTERS_H
+#define MONSTERS_H
 
-#include "display.h"
+#include <stdlib.h>
+#include "displayChars.h"
 /*****************************************************************************
    Used to represent the characteristics of a monster
  *****************************************************************************/
 typedef struct {
-    Glyph *glyph;
+    int glyph;
     int x;
     int y;
 } Monster;
 /******************************************************************************
    Allocate memory for and initialize the fields of a new Monster
  ******************************************************************************/
-Monster *newMonster(int symbol, char *style, int x, int y) {
+Monster *newMonster(int glyph, int x, int y) {
     Monster *monster = (Monster*)malloc(sizeof(Monster));
     if (monster == NULL) {
-    fprintf(stderr, "ERROR - failed to allocate Monster memory - Terminating...\n");
-    exit(1);
+        fprintf(stderr, "ERROR - failed to allocate Monster memory - Terminating...\n");
+        exit(1);
     }
 
-    monster->glyph = newGlyph(symbol, style);
+    monster->glyph = glyph;
     monster->x = x;
     monster->y = y;
 
@@ -37,16 +38,16 @@ Monster *newMonster(int symbol, char *style, int x, int y) {
   Call upon the newMonster function to generate a new player monster
  ******************************************************************************/
 Monster *newPlayer(int x, int y) {
-    Monster *player = newMonster(PLAYER, INTENSE F_GREEN, x, y);
+    Monster *player = newMonster(PLAYER, x, y);
     return player;
 }
 /******************************************************************************
   Call upon the newMonster function to generate a new fungus monster
  ******************************************************************************/
 Monster *newFungus(int x, int y) {
-    Monster *fungus = newMonster(FUNGUS, NORMAL F_GREEN, x, y);
+    Monster *fungus = newMonster(FUNGUS, x, y);
     return fungus;
 }
 
-#endif /* MONSTER_RESOURCES_H */
+#endif /* MONSTERS_H */
 
--- test.c
+++ test.c
@@ -9,76 +9,200 @@
         -Wall -ansi -std=c99
  ******************************************************************************/
 #include <stdio.h> /* printf() */
-#include "resources/display.h" /* ansi escape codes, character codes, Glyph */
+#include "resources/displayChars.h" /* character codes */
 #include "resources/monsters.h" /* Monster */
-#include "resources/maps.h" /* Tile, map printing */
 #include "resources/kbhit.h" /* kbhit(), getch() */
-#include "resources/movement.h" /* various move methods */
+/******************************************************************************
+******************************************************************************/
+#define MAP_WIDTH   45
+#define MAP_HEIGHT  18
+/******************************************************************************
+ * set of available user commands
+ ******************************************************************************/
+enum commands {
+    MV_N='8', MV_E='6', MV_S='2', MV_W='4',
+    MV_NW='7', MV_NE='9', MV_SW='1', MV_SE='3'
+};
+/******************************************************************************
+******************************************************************************/
+void move(Monster *monster, int dir, Monster *monsters[MAP_WIDTH][MAP_HEIGHT]);
 /******************************************************************************
  ******************************************************************************/
 int main() {
-    Tile *map[MAP_WIDTH][MAP_HEIGHT]; /* stores a map of tiles */
+    /* Let's try to use a different method to represent the map.
+     * I will try to use multiple different arrays, one for tiles,
+     * one for monsters, and one for items */
+    int tiles[MAP_WIDTH][MAP_HEIGHT];
+    Monster *monsters[MAP_WIDTH][MAP_HEIGHT];
 
     Monster *player; /* represents the player's current location */
     Monster *fungus; /* a fungus used to test a portion of movement */
 
     int x, y; /* stores coordinates on the map */
-    int input; /* stores the user's next command */
-    int updated; /* "boolean" representing whether the map has been updated */
+    char input; /* input character from the user */
+    short term; /* terminates execution when true */
+    short update; /* determine whether the map needs to be reprinted */
 
 
-    /* create a map of all floors */
+    /* generate a map of all floor tiles and no monsters */
     for (x = 0; x < MAP_WIDTH; x++) {
         for (y = 0; y < MAP_HEIGHT; y++) {
-            map[x][y] = newTile(FLOOR, INTENSE F_WHITE B_BLACK);
+            tiles[x][y] = FLOOR;
+            monsters[x][y] = NULL;
         }
     }
 
-    /* generate a new player monster at 0,0 */
+    /* initialize a couple monsters to add to the map */
     player = newPlayer(0, 0);
-    map[player->x][player->y] -> monster = player;
+    monsters[0][0] = player;
+    fungus = newFungus(10, 10);
+    monsters[10][10] = fungus;
 
-    /* generate a new fungus monster at 5,7 */
-    fungus = newFungus(5, 7);
-    map[fungus->x][fungus->y] -> monster = fungus;
-
-    /* print the map */
+    /* clear the screen */
     system("clear");
-    printMap(map);
 
-    do {
-        /* begin listening for user input */
-        while (kbhit() == 0) {
-            ;
+    /* print the current map */
+    for (y = 0; y < MAP_HEIGHT; y++) {
+        printf("\n ");
+        for (x = 0; x < MAP_WIDTH; x++) {
+            /* display precedence: monster > tile */
+            if (monsters[x][y] != NULL) {
+                printf("%c", monsters[x][y] -> glyph);
+            } else {
+                printf("%c", tiles[x][y]);
+            }
         }
+    }
+    printf("\n\n");
+
+    /* listen for user input until an x is entered */
+    while (kbhit() != -1) {
         input = getch();
 
-        /* assume the user entered a valid command and the map was updated */
-        updated = 1;
+        update = 1;
 
+        /* execute the appropriate action for the command */
         switch (input) {
-            /* If the user entered a move command,
-               move the player in the appropriate direction. */
-            case MV_N: moveNorth(player, map); break;
-            case MV_E: moveEast(player, map); break;
-            case MV_S: moveSouth(player, map); break;
-            case MV_W: moveWest(player, map); break;
-            case MV_NW: moveNorthwest(player, map); break;
-            case MV_NE: moveNortheast(player, map); break;
-            case MV_SW: moveSouthwest(player, map); break;
-            case MV_SE: moveSoutheast(player, map); break;
-
-            /* if the user entered an invalid command, the map is not updated */
-            default: printf(RESET "You typed: %c\n\n", input);
-                     updated = 0;
+            /* movement commands */
+            case MV_N:
+            case MV_E:
+            case MV_S:
+            case MV_W:
+            case MV_NW:
+            case MV_NE:
+            case MV_SE:
+            case MV_SW:
+                move(player, input, monsters);
+                break;
+            /* terminate execution */
+            case 'x':
+                term = 1;
+                break;
+            default:
+                printf("You entered: %c\n\n", input);
+                update = 0;
+                break;
         }
 
-        if (updated) {
+        /* user terminated input */
+        if (term == 1) {
             system("clear");
-            printMap(map);
+            break;
         }
-    } while (input != 'x');
+
+        /* only reprint the map if it was updated */
+        if (update == 1) {
+            system("clear");
+
+            /* print the current map */
+            for (y = 0; y < MAP_HEIGHT; y++) {
+                printf("\n ");
+                for (x = 0; x < MAP_WIDTH; x++) {
+                    /* print the symbol of any monster on the tile */
+                    if (monsters[x][y] != NULL) {
+                        printf("%c", monsters[x][y] -> glyph);
+                    } else {
+                        printf("%c", tiles[x][y]);
+                    }
+                }
+            }
+            printf("\n\n");
+        }
+    }
 
     return 0;
+}
+/******************************************************************************
+ * moves the provided monster to the specified direction on the provided map
+ ******************************************************************************/
+void move(Monster *monster, int dir, Monster *monsters[MAP_WIDTH][MAP_HEIGHT]) {
+    /* get current location of the monster */
+    int x = monster->x;
+    int y = monster->y;
+
+    /* Perform the move operation in the appropriate direction by first
+     * checking that the destination is free and then moving the monster */
+    switch (dir) {
+        case MV_N:
+            if (y-1 >= 0 && monsters[x][y-1] == NULL) {
+                monster->y = y-1;;
+                monsters[x][y-1] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_E:
+            if (x+1 < MAP_WIDTH && monsters[x+1][y] == NULL) {
+                monster->x = x+1;;
+                monsters[x+1][y] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_S:
+            if (y+1 < MAP_HEIGHT && monsters[x][y+1] == NULL) {
+                monster->y = y+1;;
+                monsters[x][y+1] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_W:
+            if (x-1 >= 0 && monsters[x-1][y] == NULL) {
+                monster->x = x-1;;
+                monsters[x-1][y] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_NW:
+            if (x-1 >= 0 && y-1 >= 0 && monsters[x-1][y-1] == NULL) {
+                monster->x = x-1;;
+                monster->y = y-1;;
+                monsters[x-1][y-1] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_NE:
+            if (x+1 < MAP_WIDTH && y-1 >= 0 && monsters[x+1][y-1] == NULL) {
+                monster->x = x+1;;
+                monster->y = y-1;;
+                monsters[x+1][y-1] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_SE:
+            if (x+1 < MAP_WIDTH && y+1 < MAP_HEIGHT && monsters[x+1][y+1] == NULL) {
+                monster->x = x+1;;
+                monster->y = y+1;;
+                monsters[x+1][y+1] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+        case MV_SW:
+            if (x-1 >= 0 && y+1 < MAP_HEIGHT && monsters[x-1][y+1] == NULL) {
+                monster->x = x-1;;
+                monster->y = y+1;;
+                monsters[x-1][y+1] = monster;
+                monsters[x][y] = NULL;
+            }
+            break;
+    }
 }
 
